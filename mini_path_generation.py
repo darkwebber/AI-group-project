ca4c6bd10bec319aa0e7c62c03236bbde8f53a53
darkwebber <darkweb10052003@gmail.com>
2021-11-06 04:32:59 +0530

    made mini paths better along with a file to collect them

--- mini_path_generation.py
+++ mini_path_generation.py
@@ -28,24 +28,40 @@ class Obj:
         for i in range(2):
             nil.append(int(nlist[i][0]))
         return nil
-def mpath(obj,mat,mini=[],i=0):
+def mpath(obj,mat,lent,mini=[],i=0):
         if i==0:
             mini.append(obj.pos)
             #print(f'func {mini} ini')
         if obj.cord(mat)[0]==0 or obj.cord(mat)[1] == 0 or obj.cord(mat)[1]==np.shape(mat)[1]-1 or obj.cord(mat)[0]==np.shape(mat)[0]-1:
-            obj=Obj(mini[0])
-            return mini
+            if i<lent:
+                k=random.choice(obj.nbour(mat))
+                obj=Obj(k)
+                mini.append(k)
+                return mpath(obj, mat,lent , mini,i+1)
+            if i==lent:
+                obj=Obj(mini[0])
+                return mini
+            if i>lent:
+                obj=Obj(mini[0])
+                return None
         #print(f'func {mini} fini')
         mini.append(random.choice(obj.nbour(mat)))
         #print(f'func {mini} inipini')
         obj=Obj(mini[len(mini)-1])
-        i+=1
-        return mpath(obj,mat,mini,i)
-def minipath(obj,mat,gen_size):
+        return mpath(obj,mat,lent,mini,i+1)
+def minipath(obj,mat,lent,gen_size):
     minipaths=[]
-    for i in range(gen_size):
-        minipaths.append(mpath(obj,mat,[]))
-    return np.array(minipaths)
+    open('minipaths_gen1.txt','w').close()
+    f=open('minipaths_gen1.txt','a')
+    i=0
+    while i<gen_size:
+        p=mpath(obj,mat,lent,[])
+        if p!=None:
+            i+=1
+            minipaths.append(p)
+            f.write(str(p)+"\n")
+    f.close()
+    return minipaths
 
 file=open('mat.txt', 'r')
 lines=file.readlines()
@@ -57,9 +73,9 @@ file.close()
 #if mat[x][y] is our location then
     #mat[X=x+-1][Y=y+-1] for all X,Y>=0 and X<l and Y<b
 gen_size=int(input("Enter the gen size"))
-
+lent=int(input("Length of path"))
 vc=Obj(random.randrange(1,51))
-print(minipath(vc,mat,gen_size))
+l=minipath(vc,mat,lent,gen_size)
 print(mat)
 print(vc.pos)
 print(vc.nbour(mat))
